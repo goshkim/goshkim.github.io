916be429ce6df7ca7064454e2053df437ecc9529
goshkim <goshk1995@gmail.com>
2024-04-21 19:39:39 +0900

    Link 컴포넌트 추가

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let className: string | undefined = undefined;
+	export let path: string;
+	export let isActive: boolean | undefined = undefined;
+	export let target: string | undefined = undefined;
+	export let rel: string | undefined = undefined;
+
+	let isHover = false;
+</script>
+
+<a
+	class={className}
+	href={path}
+	{target}
+	{rel}
+	on:mouseenter={() => (isHover = true)}
+	on:mouseleave={() => (isHover = false)}
+>
+	<slot name="content"></slot>
+	{#if isActive || isHover}
+		<slot name="highlighter"></slot>
+	{/if}
+</a>

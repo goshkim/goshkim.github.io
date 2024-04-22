dd4fa8d062b80003d6ebae12546d0b61b65c6000
goshkim <goshk1995@gmail.com>
2024-04-22 16:09:43 +0900

    LogoImage 컴포넌트 추가

---
+++ src/lib/components/LogoImage.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo.png';
+
+	export let className: string | undefined = undefined;
+	export let size: number | `${number}` | undefined = 140;
+
+	const alt = '블로그 로고 이미지';
+</script>
+
+<img class={className} src={logo} {alt} width={size} height={size} />

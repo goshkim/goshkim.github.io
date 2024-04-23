25b7c703f58d9c690151295d0c47299b87a48adb
goshkim <goshk1995@gmail.com>
2024-04-23 09:57:17 +0900

    IconBox 컴포넌트 추가

---
+++ src/lib/components/IconBox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import classNames from 'classnames';
+
+	export let className: string | undefined = undefined;
+</script>
+
+<button
+	class={className ??
+		classNames(
+			'flex items-center justify-center',
+			'hover:opacity-70 hover:transform-opacity hover:duration-300 hover:ease-in-out',
+			'cursor-pointer'
+		)}
+>
+	<slot name="icon"></slot>
+</button>

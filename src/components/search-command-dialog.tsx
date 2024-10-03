42d8ccaeee26ef409a747c04cb6f4ae00eded23b
goshkim <goshk1995@gmail.com>
2024-10-03 20:57:20 +0900

    search-command-dialog 컴포넌트 추가

---
+++ src/components/search-command-dialog.tsx
@@ -0,0 +1,64 @@
+import { Search } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
+import { Link } from '@/components/ui/link';
+
+interface SearchCommandDialogProps {
+  posts: { slug: string; data: { title: string } }[];
+}
+
+// TODO: 단축키 ui 표시
+const SearchCommandDialog = ({ posts }: SearchCommandDialogProps) => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === '/') {
+        e.preventDefault();
+        setOpen(true);
+      }
+    };
+
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
+  return (
+    <>
+      <button
+        type="button"
+        aria-label="검색"
+        onClick={() => setOpen(true)}
+        className="rounded-full bg-transparent p-2.5 outline-none hover:bg-transparent/5 dark:hover:bg-default-50/5"
+      >
+        <Search strokeWidth="2.5" className="size-5" />
+        <span className="sr-only">검색</span>
+        {/* <CommandShortcut>/</CommandShortcut> */}
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="게시글 제목을 입력해 주세요" className="text-base" />
+        <CommandList>
+          <CommandEmpty className="py-6 text-center text-base font-medium">
+            검색 결과가 없습니다.
+          </CommandEmpty>
+          {posts.map(({ slug, data: { title } }) => (
+            <CommandItem key={slug} asChild>
+              <Link href={`/${slug}`} underline="none" className="cursor-pointer text-base">
+                {title}
+              </Link>
+            </CommandItem>
+          ))}
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
+
+export default SearchCommandDialog;

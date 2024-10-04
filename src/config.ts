83a6f024a729f6e24af53888943a5e16aaf446af
goshkim <goshk1995@gmail.com>
2024-10-04 11:27:24 +0900

    프로필 정보 추가

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,10 @@ export const SITE = {
   description: '학습한 내용을 정리하기 위한 공간입니다.',
   image: '/site.webp',
 };
+
+export const PROFILE = {
+  username: 'goshkim',
+  email: 'goshk1995@gmail.com',
+  githubUrl: 'https://github.com/goshkim',
+  picture: 'https://github.com/goshkim.png',
+};
